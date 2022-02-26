e16cb340ec4a859c964306b8bb10f11d09a2feb2
artur <artur.oliveirasantiago@gmail.com>
2022-02-26 00:00:05 -0300

    Adicionado cores e estilos globais

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -3,20 +3,30 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
 
   :root {
-      --primary-light-3: #ECEFF1;
-      --primary-light-4: #F5F7F8;
-      --primary-color: #A3B1BA;
-      --primary-color-dark-1: #828D94;
-      --primary-color-dark-2: #51585D;
-      --primary-color-dark-3: #828D94;
+      --primary-light-4: #F7FBFD;
+      --primary-light-3: #C2E1EF;
+      --primary-color: #20607E;
+      --primary-color-dark-1: #1A4E65;
+      --primary-color-dark-2: #153E51;
+      --primary-color-dark-3: #102F3D;
   } 
 
   body {
     margin: 0;
     padding: 0;
-    background: #F5F7F8;
+    background: var(--primary-light-4);
     box-sizing: border-box;
     font-family: sans-serif;
+  }
+
+  a {
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
   }
 
 `
